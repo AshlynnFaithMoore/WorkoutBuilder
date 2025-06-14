84a20166f658cb221595282d712690694e9dd12b
Ashlynn <ashlynnfaith22@gmail.com>
2025-06-13 22:23:49 -0700

    Initial Commit

---
+++ WorkoutBuilderAppTests/WorkoutBuilderAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutBuilderAppTests.swift
+//  WorkoutBuilderAppTests
+//
+//  Created by Ashlynn Moore on 6/13/25.
+//
+
+import Testing
+@testable import WorkoutBuilderApp
+
+struct WorkoutBuilderAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
